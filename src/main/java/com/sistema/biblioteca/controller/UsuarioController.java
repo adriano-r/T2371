63a8848289890f991a0d5b02d6110f81bb61c126
Adriano Rosa <adrianorosa9974@gmail.com>
2023-03-10 09:36:35 -0300

    FIX: update mysql driver in application.properties

--- src/main/java/com/sistema/biblioteca/controller/UsuarioController.java
+++ src/main/java/com/sistema/biblioteca/controller/UsuarioController.java
@@ -1,31 +1,33 @@
 package com.sistema.biblioteca.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sistema.biblioteca.exception.UsuarioNotFoundExecption;
 import com.sistema.biblioteca.repository.UsuarioRepository;
 import com.sistema.biblioteca.service.UsuarioService;
-import com.sistema.biblioteca.usuario.DadosAtualizacaoUsuario;
-import com.sistema.biblioteca.usuario.DadosCadastroUsuario;
 import com.sistema.biblioteca.usuario.Usuario;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import lombok.var;
 
 @RestController
 @CrossOrigin("*")
@@ -34,9 +36,9 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	private UsuarioService usuarioService;
-	
+
 	public UsuarioController(UsuarioService usuarioService) {
 		this.usuarioService = usuarioService;
 	}
@@ -73,7 +75,7 @@ public class UsuarioController {
 //		usuarioService.atualizarUsuario(dados);
 //		return ResponseEntity.status(201).build();
 //	}
- 
+
 	@DeleteMapping("/{id}")
 	@Transactional
 	public ResponseEntity<?> excluir(@PathVariable Long id) {
@@ -84,14 +86,28 @@ public class UsuarioController {
 //		var usuario = repository.getReferenceById(id);
 //		usuario.excluir();
 	}
-	
+
 	@PostMapping("/login")
-	public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> validarSenha(@RequestBody @Valid Usuario usuario) {
 		Boolean valid = usuarioService.validarSenha(usuario);
-		if(!valid) {
+		if (!valid) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 		return ResponseEntity.status(200).build();
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+
+		return errors;
 	}
 
 }
--- src/main/java/com/sistema/biblioteca/usuario/Usuario.java
+++ src/main/java/com/sistema/biblioteca/usuario/Usuario.java
@@ -6,26 +6,40 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
-@Getter
+@Data
+//@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+//@EqualsAndHashCode(of = "id")
 //@Embeddable
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "O nome é obrigatório!")
+	@Size(min = 3, message = "O nome deve ter no minimo 3 caracteres!")
 	private String nome;
+	
+	@Email(message = "Insira um email válido!")
+	@NotBlank(message = "O email é obrigatório!")
 	private String email;
+	
+	@NotBlank(message = "A senha é obrigatória!")
 	private String senha;
+	
 	private Boolean isAdmin;
 
 	public Usuario(DadosCadastroUsuario dados) {
